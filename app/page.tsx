de5c2c023a1d5eb05db6392e13f5282e794e4842
nReus/sviiseven <nreus.gs@gmail.com>
2024-02-26 15:51:49 -0500

    remove that thing

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,14 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Latest project:&nbsp;
-          <code className="font-mono font-bold">
-            <a href="https://eiga.nakanoista.cc/">Eiga</a>
-          </code>
+        <p className="fixed">
+          
         </p>
       </div>
 
       <div>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] light"
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark"
           src="https://eiga.nakanoista.cc/static/nakanoista_darkbg.svg"
           alt="Nakanoista Logo"
           width={500}
@@ -38,7 +35,7 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[50ch] text-sm opacity-50`}>
+          <p className={`m-0 max-w-[60ch] text-sm opacity-50`}>
             Join our discord where we will be providing updates on our projects.
           </p>
         </a>
@@ -54,7 +51,7 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[50ch] text-sm opacity-50`}>
+          <p className={`m-0 max-w-[60ch] text-sm opacity-50`}>
             Project page for &quot;The Quintessential Quintuplets the Movie: Five Memories of My Time with You&quot;
           </p>
         </a>
