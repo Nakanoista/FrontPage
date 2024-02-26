5802cbd5f4772b746b8fb45cd64e0188706663cf
nReus/sviiseven <nreus.gs@gmail.com>
2024-02-26 15:12:30 -0500

    Update background

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import './main.css'
 
 export default function Home() {
   return (
@@ -12,12 +13,13 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#ff0191] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div>
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] light"
           src="https://eiga.nakanoista.cc/static/nakanoista_darkbg.svg"
           alt="Nakanoista Logo"
           width={500}
+          height={150}
           priority
         />
       </div>
@@ -44,7 +46,6 @@ export default function Home() {
         <a
           href="https://eiga.nakanoista.cc/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
