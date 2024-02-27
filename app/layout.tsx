306d65eb68ff643d8967bf84d20502b0118abde3
nReus/sviiseven <nreus.gs@gmail.com>
2024-02-27 09:30:46 -0500

    testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Modal from "./Modal";
+import Modal from "../components/Modal";
 
 const inter = Inter({ subsets: ["latin"] });
 
